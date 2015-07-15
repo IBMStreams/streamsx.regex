90e9290edf0bb068f8e90cbfe1501d455c8ad8e1
leongor <leongor@gmail.com>
2015-07-15 19:48:14 +0300

    Redesigned native functions implementation.

--- com.ibm.streamsx.regex/impl/include/Regex.h
+++ com.ibm.streamsx.regex/impl/include/Regex.h
@@ -20,6 +20,7 @@ using namespace std;
 
 namespace regex {
 
+	template<typename Index>
 	inline RE2::Options& getRE2Options(int maxmem){
 		static RE2::Options options;
 		options.set_log_errors(false);
@@ -27,21 +28,33 @@ namespace regex {
 		return options;
 	}
 
-	inline RE2& getRE2(const string & pattern, int maxmem){
-		static RE2 regex(pattern, getRE2Options(maxmem));
+	template<typename Index>
+	inline RE2& getRE2(const string & pattern="", int maxmem=1000000){
+		static RE2 regex(pattern, getRE2Options<Index>(maxmem));
 		return regex;
 	}
 
-	inline bool regexFullMatch(const string & str, const string & pattern="", int maxmem=1000000){
-		return RE2::FullMatch(str, getRE2(pattern, maxmem)) == 1;
+	template<typename Index>
+	inline void regexCompile(const SPL::rstring & pattern, const Index & patternIndex, int maxmem=1000000){
+		getRE2<Index>(pattern, maxmem);
 	}
 
-	inline bool regexPartialMatch(const string & str, const string & pattern="", int maxmem=1000000){
-		return RE2::PartialMatch(str, getRE2(pattern, maxmem)) == 1;
+	inline bool regexFullMatch(const string & str, const SPL::rstring & pattern){
+		return RE2::FullMatch(str, pattern) == 1;
 	}
 
-	inline bool regexSimpleMatch(const string & str, const string & pattern){
+	template<typename Index>
+	inline bool regexFullMatch(const string & str, const Index & patternIndex){
+		return RE2::FullMatch(str, getRE2<Index>()) == 1;
+	}
+
+	inline bool regexPartialMatch(const string & str, const SPL::rstring & pattern){
 		return RE2::PartialMatch(str, pattern) == 1;
+	}
+
+	template<typename Index>
+	inline bool regexPartialMatch(const string & str, const Index & patternIndex){
+		return RE2::PartialMatch(str, getRE2<Index>()) == 1;
 	}
 }
 
