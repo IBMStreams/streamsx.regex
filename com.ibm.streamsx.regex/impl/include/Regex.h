2fdc978c6ecb4e16b10a38cf7ba1104a87cfae12
Leonid Gorelik <leongor@gmail.com>
2015-11-23 16:15:45 +0200

    The functions now work on operator scope. For PE scope static functions are introduced.

--- com.ibm.streamsx.regex/impl/include/Regex.h
+++ com.ibm.streamsx.regex/impl/include/Regex.h
@@ -20,23 +20,46 @@ using namespace std;
 
 namespace regex {
 
-	template<typename Index>
+	namespace {
+		struct OperatorInstance {};
+	}
+
+	template<typename Index, typename OP>
 	inline RE2::Options& getRE2Options(int maxmem){
 		static RE2::Options options;
-		options.set_log_errors(false);
+		options.set_log_errors(true);
 		options.set_max_mem(maxmem);
 		return options;
 	}
 
 	template<typename Index>
+	inline RE2::Options& getRE2OptionsStatic(int maxmem){
+		static RE2::Options options;
+		options.set_log_errors(true);
+		options.set_max_mem(maxmem);
+		return options;
+	}
+
+	template<typename Index, typename OP>
 	inline RE2& getRE2(const string & pattern="", int maxmem=1000000){
-		static RE2 regex(pattern, getRE2Options<Index>(maxmem));
+		static RE2 regex(pattern, getRE2Options<Index,OP>(maxmem));
+		return regex;
+	}
+
+	template<typename Index>
+	inline RE2& getRE2Static(const string & pattern="", int maxmem=1000000){
+		static RE2 regex(pattern, getRE2OptionsStatic<Index>(maxmem));
 		return regex;
 	}
 
 	template<typename Index>
 	inline void regexCompile(const SPL::rstring & pattern, const Index & patternIndex, int maxmem=1000000){
-		getRE2<Index>(pattern, maxmem);
+		getRE2<Index,OperatorInstance>(pattern, maxmem);
+	}
+
+	template<typename Index>
+	inline void regexCompileStatic(const SPL::rstring & pattern, const Index & patternIndex, int maxmem=1000000){
+		getRE2Static<Index>(pattern, maxmem);
 	}
 
 	inline bool regexFullMatch(const string & str, const SPL::rstring & pattern){
@@ -45,7 +68,12 @@ namespace regex {
 
 	template<typename Index>
 	inline bool regexFullMatch(const string & str, const Index & patternIndex){
-		return RE2::FullMatch(str, getRE2<Index>()) == 1;
+		return RE2::FullMatch(str, getRE2<Index,OperatorInstance>()) == 1;
+	}
+
+	template<typename Index>
+	inline bool regexFullMatchStatic(const string & str, const Index & patternIndex){
+		return RE2::FullMatch(str, getRE2Static<Index>()) == 1;
 	}
 
 	inline bool regexPartialMatch(const string & str, const SPL::rstring & pattern){
@@ -54,7 +82,12 @@ namespace regex {
 
 	template<typename Index>
 	inline bool regexPartialMatch(const string & str, const Index & patternIndex){
-		return RE2::PartialMatch(str, getRE2<Index>()) == 1;
+		return RE2::PartialMatch(str, getRE2<Index,OperatorInstance>()) == 1;
+	}
+
+	template<typename Index>
+	inline bool regexPartialMatchStatic(const string & str, const Index & patternIndex){
+		return RE2::PartialMatch(str, getRE2Static<Index>()) == 1;
 	}
 }
 
